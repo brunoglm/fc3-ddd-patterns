b613a0f73089ea99f9c803954c9d23a30a1fbb69
Bruno Gabriel <bruno.glm_@hotmail.com>
2022-04-20 01:15:01 -0300

    finalizando o projeto

--- src/domain/repository/customerRepositoryInterface.ts
+++
@@ -1,5 +0,0 @@
-import RepositoryInterface from "./repositoryInterface";
-import Customer from "../entity/customer";
-
-export default interface CustomerRepositoryInterface
-    extends RepositoryInterface<Customer> { }
--- src/domain/repository/orderRepositoryInterface.ts
+++
@@ -1,5 +0,0 @@
-import RepositoryInterface from './repositoryInterface'
-import Order from '../entity/order'
-
-export default interface OrderRepositoryInterface
-  extends RepositoryInterface<Order> {}
